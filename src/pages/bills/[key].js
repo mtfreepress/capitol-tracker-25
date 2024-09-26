868f03d9694bfeba404246287f96f2e9c963f903
Jacob Olness <jolness1@gmail.com>
2024-09-26 14:10:22 -0600

    oops forgot to add stuff

---
+++ src/pages/bills/[key].js
@@ -0,0 +1,28 @@
+import BillCard from '../../components/BillCard';
+import bills from '../../data/bills.json';
+
+export async function getStaticPaths() {
+    const paths = bills.map(bill => ({
+        params: { key: bill.key },
+    }));
+
+    return { paths, fallback: false };
+}
+
+export async function getStaticProps({ params }) {
+    const bill = bills.find(bill => bill.key === params.key);
+
+    return { props: { bill } };
+}
+
+const BillPage = ({ bill }) => {
+    if (!bill) return <div>Bill not found</div>;
+
+    return (
+        <div>
+            <BillCard bill={bill} />
+        </div>
+    );
+};
+
+export default BillPage;
